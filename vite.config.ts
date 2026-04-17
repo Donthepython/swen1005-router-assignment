bb8fba7e1b4b36d525aa2cc6af5e663b90dfe362
DonPaul <donpaulsamuel@gmail.com>
2026-04-17 15:29:47 -0400

    Link fix attempt #1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/swen1005-router-assignment/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
