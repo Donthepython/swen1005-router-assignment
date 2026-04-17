46566b1eaffac502b78c63e426d5623a5bd57fb4
DonPaul <donpaulsamuel@gmail.com>
2026-04-17 16:48:53 -0400

    fix#4

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/swen1005-router-assignment/",
+export default defineConfig(({ mode }) => ({//changes recommended by chatgpt as my github site was having loading errors
+  base:
+    mode === "development"
+      ? "/swen1005-router-assignment"
+      : "/swen1005-router-assignment/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
-});
+}));
